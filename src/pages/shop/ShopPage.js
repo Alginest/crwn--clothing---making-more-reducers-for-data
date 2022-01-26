9b2a61270fa4b9632e94e7099d9475aae1bcc37c
ado durak <76266634+Alginest@users.noreply.github.com>
2022-01-26 15:25:52 +0100

    Add files via upload

---
+++ src/pages/shop/ShopPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview";
+
+const ShopPage = () => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
+};
+
+export default ShopPage;
